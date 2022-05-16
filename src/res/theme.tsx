44c95ec237cf65976b9239c6fd31279da17ab88f
Berzan Yildiz <17531789+bmikaili@users.noreply.github.com>
2022-05-16 13:37:44 +0200

    add themes'

--- src/res/theme.tsx
+++ src/res/theme.tsx
@@ -1,22 +1,24 @@
 import {createTheme} from '@shopify/restyle';
 
 const palette = {
-  purpleLight: '#8C6FF7',
-  purplePrimary: '#5A31F4',
-  purpleDark: '#3F22AB',
+  blue: '#01BAEF',
+  yellow: '#FED90F',
+  red: '#FF1053',
 
-  greenLight: '#56DCBA',
-  greenPrimary: '#0ECD9D',
-  greenDark: '#0A906E',
-
-  black: '#0B0B0B',
-  white: '#F0F2F3',
+  black: '#040F16',
+  white: '#FFFFFF',
 };
 
 const theme = createTheme({
   colors: {
     mainBackground: palette.white,
-    cardPrimaryBackground: palette.purplePrimary,
+    primaryText: palette.black,
+    createButton: palette.black,
+    shadow: palette.black,
+    emoji: palette.yellow,
+    primaryCard: palette.blue,
+    secondaryCard: palette.yellow,
+    tertiaryCard: palette.red
   },
   spacing: {
     s: 8,
@@ -28,6 +30,53 @@ const theme = createTheme({
     phone: 0,
     tablet: 768,
   },
+  textVariants: {
+    header: {
+      fontFamily: 'ShopifySans-Bold',
+      fontWeight: 'bold',
+      fontSize: 34,
+      lineHeight: 42.5,
+      color: 'primaryText',
+    },
+    subheader: {
+      fontFamily: 'ShopifySans-SemiBold',
+      fontWeight: '600',
+      fontSize: 28,
+      lineHeight: 36,
+      color: 'primaryText',
+    },
+    body: {
+      fontFamily: 'ShopifySans',
+      fontSize: 16,
+      lineHeight: 24,
+      color: 'primaryText',
+    },
+  },
+  cardVariants: {
+    defaults: {
+      // We can define defaults for the variant here.
+      // This will be applied after the defaults passed to createVariant and before the variant defined below.
+    },
+    regular: {
+      // We can refer to other values in the theme here, and use responsive props
+      padding: {
+        phone: 's',
+        tablet: 'm',
+      },
+      backgroundColor: 'primaryCard',
+    },
+    elevated: {
+      padding: {
+        phone: 's',
+        tablet: 'm',
+      },
+      shadowColor: 'shadow',
+      shadowOpacity: 0.2,
+      shadowOffset: {width: 0, height: 5},
+      shadowRadius: 15,
+      elevation: 5,
+    }
+  }
 });
 
 export type Theme = typeof theme;
