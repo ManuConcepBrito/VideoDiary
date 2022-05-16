1cf2ef254ed90e575e606b0d0636911b8dd6e396
Manuel Concepcion Brito <manuel@Manuels-MacBook-Pro.local>
2022-05-16 12:03:42 +0200

    update file structure

---
+++ src/res/colors.tsx
@@ -0,0 +1,10 @@
+const colors = {
+    buttons: {
+        "blue": "#01BAEF",
+        "yellow": "#FED90F",
+        "red": "#FF1053",
+        "black": "#040F16",
+        "emojiYellow": "#FED90F"
+    },
+
+}
