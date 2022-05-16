b0d75593322617a5c5580c5f5ef4b200d6b4d276
Manuel Concepcion Brito <manuel@Manuels-MacBook-Pro.local>
2022-05-16 15:42:59 +0200

    create default componenets

--- src/components/atoms/HellowWorld.tsx
+++ src/components/atoms/HellowWorld.tsx
@@ -2,11 +2,14 @@
 import React from 'react';
 import {View} from "react-native"
 import Text from "./Text";
+import Button from "./Button";
 // functional component with hello world in the center of the page
 export default function HelloWorld() {
+    // function that alerts when the button is pressed
+    const onPress = () => alert("I am pressed");
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text variant="header">Hello World!!</Text>
+            <Button label="Fuck you bitches" onPress={onPress}/>
         </View>
     );
 }
