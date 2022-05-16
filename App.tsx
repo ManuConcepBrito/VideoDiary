ebab58ec3ee649a6c7500f5348f5bfe497f4d5d2
Berzan Yildiz <17531789+bmikaili@users.noreply.github.com>
2022-05-16 13:00:26 +0200

    styles

--- App.tsx
+++ App.tsx
@@ -1,108 +1,28 @@
-import React from "react";
-import { StyleSheet, Text, View, AppRegistry } from "react-native";
+// In App.js in a new project
 
-import { NativeRouter, Route, Link } from "react-router-native";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import VideoList from './src/components/VideoList';
+import {ThemeProvider} from '@shopify/restyle';
+import theme from './src/res/theme';
 
-const Home = () => <Text style={styles.header}>Home</Text>;
+const Stack = createNativeStackNavigator();
 
-const About = () => <Text style={styles.header}>About</Text>;
-
-const Topic = ({ match }) => (
-  <Text style={styles.topic}>{match.params.topicId}</Text>
-);
-
-const Topics = ({ match }) => (
-  <View>
-    <Text style={styles.header}>Topics</Text>
-    <View>
-      <Link
-        to={`${match.url}/rendering`}
-        style={styles.subNavItem}
-        underlayColor="#f0f4f7"
-      >
-        <Text>Rendering with React</Text>
-      </Link>
-      <Link
-        to={`${match.url}/components`}
-        style={styles.subNavItem}
-        underlayColor="#f0f4f7"
-      >
-        <Text>Components</Text>
-      </Link>
-      <Link
-        to={`${match.url}/props-v-state`}
-        style={styles.subNavItem}
-        underlayColor="#f0f4f7"
-      >
-        <Text>Props v. State</Text>
-      </Link>
-    </View>
-
-    <Route path={`${match.path}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.path}
-      render={() => (
-        <Text style={styles.topic}>Please select a topic.</Text>
-      )}
-    />
-  </View>
-);
-
-const App = () => (
-  <NativeRouter>
-    <View style={styles.container}>
-      <View style={styles.nav}>
-        <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
-          <Text>Home</Text>
-        </Link>
-        <Link
-          to="/about"
-          underlayColor="#f0f4f7"
-          style={styles.navItem}
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>{
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="VideoList"
+          screenOptions={{
+          headerShown: false
+          }}
         >
-          <Text>About</Text>
-        </Link>
-        <Link
-          to="/topics"
-          underlayColor="#f0f4f7"
-          style={styles.navItem}
-        >
-          <Text>Topics</Text>
-        </Link>
-      </View>
+          <Stack.Screen name="VideoList" component={VideoList} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    }</ThemeProvider>
+  );
+}
 
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
-    </View>
-  </NativeRouter>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 25,
-    padding: 10
-  },
-  header: {
-    fontSize: 20
-  },
-  nav: {
-    flexDirection: "row",
-    justifyContent: "space-around"
-  },
-  navItem: {
-    flex: 1,
-    alignItems: "center",
-    padding: 10
-  },
-  subNavItem: {
-    padding: 5
-  },
-  topic: {
-    textAlign: "center",
-    fontSize: 15
-  }
-});
-
-AppRegistry.registerComponent("MyApp", () => App);
+export default App;
---
+++ src/components/VideoList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import theme from "../res/theme";
+
+const VideoList = () => {
+  return (
+    <View style={[styles.container, {
+      // Try setting `flexDirection` to `"row"`.
+      flexDirection: "column"
+    }]}>
+      <View style={{ flex: 1, backgroundColor: theme.colors.cardPrimaryBackground }} />
+      <View style={{ flex: 2, backgroundColor: "darkorange" }} />
+      <View style={{ flex: 3, backgroundColor: "green" }} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
+
+export default VideoList;
