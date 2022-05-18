4ed07dcb57f38cee18a2661a7d58322850cd64a5
Berzan Yildiz <17531789+bmikaili@users.noreply.github.com>
2022-05-18 16:45:30 +0200

    searchable

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 import { COLORS, FONTS, SPACING } from '../res/theme';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Button from './Button';
 
 const numColumns = 3;
-const data = [
+const mock = [
   { id: '0', day: 'Sat', date: '30 May' },
   { id: '1', day: 'Sat', date: '30 May' },
   { id: '2', day: 'Sat', date: '30 May' },
   { id: '3', day: 'Sat', date: '30 May' },
   { id: '4', day: 'Sat', date: '30 May' },
   { id: '5', day: 'Sat', date: '30 May' },
-  { id: '6', day: 'Sat', date: '30 May' },
+  { id: '6', day: 'Mon', date: '30 May' },
   { id: '7', day: 'Sat', date: '30 May' },
   { id: '8', day: 'Sat', date: '30 May' },
   { id: '9', day: 'Sat', date: '30 May' },
   { id: '10', day: 'Sat', date: '30 May' },
-  { id: '11', day: 'Sat', date: '30 May' },
-  { id: '12', day: 'Sat', date: '30 May' },
+  { id: '11', day: 'Sat', date: '15 May' },
+  { id: '12', day: 'Sat', date: '30 June' },
   { id: '13', day: 'Sat', date: '30 May' },
   { id: '14', day: 'Sat', date: '30 May' },
   { id: '15', day: 'Sat', date: '30 May' },
@@ -91,38 +91,71 @@ const cardStyleMap: any = {
   '2': styles.cardRed,
 };
 
-function renderHeader() {
-  return (
-    <View
-      style={{
-        paddingHorizontal: SPACING.xxs,
-        paddingBottom: SPACING.xxs,
-      }}
-    >
+const VideoList = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [query, setQuery] = useState('');
+  const [fullData, setFullData] = useState([]);
+
+  // TODO: API Call
+  useEffect(() => {
+    setData(mock);
+    setFullData(mock);
+  }, []);
+
+  const handleSearch = (text) => {
+    const formattedQuery = text.toLowerCase();
+    const filteredData = fullData.filter((entry) => {
+      console.log(entry);
+      return contains(entry, formattedQuery);
+    });
+    setData(filteredData);
+    setQuery(text);
+  };
+
+  const contains = ({ day, date }, query) => {
+    if (
+      day.toLowerCase().includes(query) ||
+      date.toLowerCase().includes(query)
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const renderHeader = () => {
+    return (
       <View
         style={{
-          position: 'absolute',
-          zIndex: 1,
-          paddingTop: SPACING.m + 2,
-          paddingLeft: SPACING.m + 3,
+          paddingHorizontal: SPACING.xxs,
+          paddingBottom: SPACING.xxs,
         }}
       >
-        <Icon color={COLORS.grey} name="search" size={20}></Icon>
+        <View
+          style={{
+            position: 'absolute',
+            zIndex: 1,
+            paddingTop: SPACING.m + 2,
+            paddingLeft: SPACING.m + 3,
+          }}
+        >
+          <Icon color={COLORS.grey} name="search" size={20}></Icon>
+        </View>
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          clearButtonMode="always"
+          value={query}
+          placeholder="Tags, Days, Dates..."
+          onChangeText={(queryText) => handleSearch(queryText)}
+          style={styles.textInput}
+        />
       </View>
-      <TextInput
-        autoCapitalize="none"
-        autoCorrect={false}
-        clearButtonMode="always"
-        value={''}
-        placeholder="Tags, Days, Dates..."
-        onChangeText={(queryText) => handleSearch(queryText)}
-        style={styles.textInput}
-      />
-    </View>
-  );
-}
+    );
+  };
 
-const VideoList = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Your Video Diary</Text>
