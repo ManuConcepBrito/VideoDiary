299c5746d646fdbedc8bdc23816e4a83a65101c7
Manuel Concepcion Brito <manuel@Manuels-MBP.lan>
2022-06-28 12:42:59 +0200

    camera working

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import { useFonts, ChakraPetch_700Bold } from '@expo-google-fonts/chakra-petch';
 
 import React, { useEffect, useState } from 'react';
 import VideoList from './src/components/VideoList';
-// import CameraScreen from './src/components/CameraScreen';
 import { CameraPage } from './src/components/camera/CameraScreen';
 import { PermissionsPage } from './src/components/camera/PermissionsPage';
 import DescribeVideo from './src/components/DescribeVideo';
@@ -12,9 +11,9 @@ import DescribeVideoAddNotes from './src/components/DescribeVideoAddNotes';
 import { Entry, Mood } from './src/store/DiaryStore';
 import EditVideo from './src/components/EditVideo';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-import * as Sentry from 'sentry-expo';
 import { Camera, CameraPermissionStatus } from 'react-native-vision-camera';
 import * as MediaLibrary from 'expo-media-library';
+import * as Sentry from 'sentry-expo';
 
 Sentry.init({
   dsn: 'https://ce22889f23a349a3a074aa325f901e11@o1278860.ingest.sentry.io/6478871',
@@ -76,7 +75,7 @@ const App = () => {
       <BottomSheetModalProvider>
         <Stack.Navigator
           initialRouteName={
-            showPermissionsPage ? 'PermissionsPage' : 'CameraPage'
+            showPermissionsPage ? 'PermissionsPage' : 'VideoList'
           }
           screenOptions={{
             headerShown: false,
